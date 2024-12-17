a9fe0e5eabd593aceee6a2d99b15ce74823476ce
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2024-12-17 12:23:01 -0500

    Refactorizando el proyecto

--- src/main/java/com/tareas/crudtareas/Repository/TareaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tareas.crudtareas.Repository;
-
-import com.tareas.crudtareas.Model.Tarea;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TareaRepository extends JpaRepository<Tarea, Long> {
-}
